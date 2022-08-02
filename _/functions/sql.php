e435d53bdec08c602c188b1bf7a9a4cd78eaa155
oualid <34518781+ouarrho@users.noreply.github.com>
2022-08-02 18:54:15 +0200

    Update sql.php

--- _/functions/sql.php
+++ _/functions/sql.php
@@ -1,20 +1,28 @@
 <?php
 
-    function SQL_INSERT($TABLE, $VALUES){
+    function SQL_INSERT( $TABLE, $VALUES ){
 
         //... 
 
     }
 
-    function SQL_UPDATE($TABLE, $SET, $WHERE=nul){
+    function SQL_UPDATE( $TABLE, $SET, $WHERE=null ){
 
         //... 
 
     }
 
-    function SQL_SELECT($COLUMN, $TABLE, $WHERE=null, $GROUP=null, $ORDER=null, $LIMIT=null){
+    function SQL_SELECT( $COLUMN, $TABLE, $WHERE=null, $GROUP=null, $ORDER=null, $LIMIT=null ){
 
-        //... 
+        global $PDO;
+
+        $select = $PDO->query( 'SELECT * FROM u' );
+
+        while( $row = $select->fetch( PDO::FETCH_ASSOC ) ):
+
+            echo $row[ 'userMail' ];
+
+        endwhile;
 
     }
 
