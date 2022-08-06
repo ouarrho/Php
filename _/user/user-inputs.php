e167aa696d219af4b23c6bfd7628b96641deb6ff
oualid <34518781+ouarrho@users.noreply.github.com>
2022-08-06 12:50:48 +0200

    Update user-inputs.php

--- _/user/user-inputs.php
+++ _/user/user-inputs.php
@@ -1,8 +1,10 @@
 <?php
 
+    declare( strict_types = 1 );
+
     class USER_INPUTS {
 
-        public function NEW_VAR( $_data ){
+        public function NEW_VAR( array $_data ){
 
             foreach($_data AS $_name):
 
@@ -12,12 +14,27 @@
 
         }
 
-        public function IF_ISSET( $_data ){
+        public function ISSET( array $_data ){
 
 
         }
 
-        public function IF_EMPTY( $_data ){
+        public function TYPE( array $_data ){
+
+
+        }
+
+        public function MIN( array $_data ){
+
+
+        }
+
+        public function MAX( array $_data ){
+
+
+        }
+
+        public function LENGTH( array $_data ){
 
 
         }
