a9ff87e86873b021c59ed348e093a54dfa4494ca
oualid <34518781+ouarrho@users.noreply.github.com>
2022-08-11 11:44:10 +0200

    --

--- _/user/user-inputs.php
+++ _/user/user-inputs.php
@@ -17,76 +17,12 @@
 */
     declare( strict_types = 1 );
 
+    require 'user-inputs-isset.php';
+
     class USER_INPUTS {
 
 
-    /**
-        *
-        * This method is bound to ISSET() method
-        *
-        * It checks if the input exists taking into consideration all the methods of submitting
-        * 
-        * If the input exists it returns true and vice versa
-        * 
-        * The return value is either true or false
-        *
-    */
-        public function ISSET_BY_METHOD( string $_METHOD,  string $_NAME ): bool {
-
-                if( $_METHOD == 'SERVER'  ): return isset( $_SERVER  [ $_NAME ] ) ? true : false;
-
-            elseif( $_METHOD == 'ENV'     ): return isset( $_ENV     [ $_NAME ] ) ? true : false;
-
-            elseif( $_METHOD == 'COOKIE'  ): return isset( $_COOKIE  [ $_NAME ] ) ? true : false;
-
-            elseif( $_METHOD == 'SESSION' ): return isset( $_SESSION [ $_NAME ] ) ? true : false;
-
-            elseif( $_METHOD == 'FILES'   ): return isset( $_FILES   [ $_NAME ] ) ? true : false;
-
-            elseif( $_METHOD == 'REQUEST' ): return isset( $_REQUEST [ $_NAME ] ) ? true : false;
-
-            elseif( $_METHOD == 'POST'    ): return isset( $_POST    [ $_NAME ] ) ? true : false;
-
-            elseif( $_METHOD == 'GET'     ): return isset( $_GET     [ $_NAME ] ) ? true : false;
-
-            endif;
-
-        }
-
-
-    /**
-        *
-        * 
-        *
-    */
-        public function ISSET( array $_DATA ){
-
-            foreach( $_DATA AS $_METHOD => $_VALUE ):
-
-                if( !empty( $_VALUE ) ):
-                
-                    foreach( $_VALUE AS $__VALUE ):
-
-                        $EXPLODE__VALUE = explode( ' AS ', $__VALUE );
-
-                        if( ! $this -> ISSET_BY_METHOD( $_METHOD, $EXPLODE__VALUE[ 0 ] ) ):
-
-                            echo "$_METHOD => {$EXPLODE__VALUE[ 0 ]} NOT FOUND! <br>";
-
-                            exit();
-
-                        endif;
-
-                    endforeach;
-
-                endif;
-
-            endforeach;
-
-            return $this;
-
-        }
-
+        use INPUT_ISSET;
 
     /**
         *
@@ -189,7 +125,77 @@
 
             global $arr_inputs_filters;
 
-            if( $_METHOD == 'GET' ):
+            if( $_METHOD == 'SERVER' ):
+
+                if( !filter_var( $_SERVER[ $_NAME ], $arr_inputs_filters[ $_ATTR[ 'type' ] ] ) ):
+
+                    echo "$_NAME invalid type";
+
+                    exit();
+
+                endif;
+
+            elseif( $_METHOD == 'ENV' ):
+
+                if( !filter_var( $_ENV[ $_NAME ], $arr_inputs_filters[ $_ATTR[ 'type' ] ] ) ):
+
+                    echo "$_NAME invalid type";
+
+                    exit();
+
+                endif;
+
+            elseif( $_METHOD == 'COOKIE' ):
+
+                if( !filter_var( $_COOKIE[ $_NAME ], $arr_inputs_filters[ $_ATTR[ 'type' ] ] ) ):
+
+                    echo "$_NAME invalid type";
+
+                    exit();
+
+                endif;
+
+            elseif( $_METHOD == 'SESSION' ):
+
+                if( !filter_var( $_SESSION[ $_NAME ], $arr_inputs_filters[ $_ATTR[ 'type' ] ] ) ):
+
+                    echo "$_NAME invalid type";
+
+                    exit();
+
+                endif;
+
+            elseif( $_METHOD == 'FILES' ):
+
+                if( !filter_var( $_FILES[ $_NAME ], $arr_inputs_filters[ $_ATTR[ 'type' ] ] ) ):
+
+                    echo "$_NAME invalid type";
+
+                    exit();
+
+                endif;
+
+            elseif( $_METHOD == 'REQUEST' ):
+
+                if( !filter_var( $_REQUEST[ $_NAME ], $arr_inputs_filters[ $_ATTR[ 'type' ] ] ) ):
+
+                    echo "$_NAME invalid type";
+
+                    exit();
+
+                endif;
+
+            elseif( $_METHOD == 'POST' ):
+
+                if( !filter_var( $_POST[ $_NAME ], $arr_inputs_filters[ $_ATTR[ 'type' ] ] ) ):
+
+                    echo "$_NAME invalid type";
+
+                    exit();
+
+                endif;
+
+            elseif( $_METHOD == 'GET' ):
 
                 if( !filter_var( $_GET[ $_NAME ], $arr_inputs_filters[ $_ATTR[ 'type' ] ] ) ):
 
@@ -202,6 +208,7 @@
             endif;
 
         }
+
 
     /**
         *
