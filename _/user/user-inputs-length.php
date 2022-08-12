c4a2c87dc3c8673a0471957ee49883fe8449eead
oualid <34518781+ouarrho@users.noreply.github.com>
2022-08-12 13:33:06 +0200

    --

--- _/user/user-inputs-length.php
+++ _/user/user-inputs-length.php
@@ -57,6 +57,37 @@
     */
         public function MAX( array $_DATA ){
 
+            foreach( $_DATA AS $_METHOD => $_VALUE ):
+
+                if( !empty( $_VALUE ) ):
+                
+                    foreach( $_VALUE AS $__VALUE ):
+
+                        $EXPLODE__VALUE = $this -> EXPLODE_VAR_ATTR( $__VALUE );
+
+                        if( isset( $EXPLODE__VALUE[ 'ATTR' ][ 'max' ] ) ):
+
+                            $_VAR = $this -> ARRAY_INPUTS_METHODS[ $_METHOD ][ $EXPLODE__VALUE[ 'NAME' ] ];
+
+                            ( is_numeric( $_VAR ) ) ? $input_length = $_VAR : $input_length = strlen( $_VAR );
+
+                            if( $input_length > $EXPLODE__VALUE[ 'ATTR' ][ 'max' ] ):
+
+                                echo "{$EXPLODE__VALUE[ 'NAME' ]} max length/value is {$EXPLODE__VALUE[ 'ATTR' ][ 'max' ]}";
+
+                                exit();
+
+                            endif;
+
+                        endif;
+
+                    endforeach;
+
+                endif;
+
+            endforeach;
+
+            return $this;
 
         }
 
@@ -67,6 +98,35 @@
     */
         public function LENGTH( array $_DATA ){
 
+            foreach( $_DATA AS $_METHOD => $_VALUE ):
+
+                if( !empty( $_VALUE ) ):
+                
+                    foreach( $_VALUE AS $__VALUE ):
+
+                        $EXPLODE__VALUE = $this -> EXPLODE_VAR_ATTR( $__VALUE );
+
+                        if( isset( $EXPLODE__VALUE[ 'ATTR' ][ 'length' ] ) ):
+
+                            $_VAR = $this -> ARRAY_INPUTS_METHODS[ $_METHOD ][ $EXPLODE__VALUE[ 'NAME' ] ];
+
+                            if( strlen( $_VAR ) != $EXPLODE__VALUE[ 'ATTR' ][ 'length' ] ):
+
+                                echo "{$EXPLODE__VALUE[ 'NAME' ]} length/value is {$EXPLODE__VALUE[ 'ATTR' ][ 'length' ]}";
+
+                                exit();
+
+                            endif;
+
+                        endif;
+
+                    endforeach;
+
+                endif;
+
+            endforeach;
+
+            return $this;
 
         }
 
--- _/user/user-inputs-type.php
+++ _/user/user-inputs-type.php
@@ -51,20 +51,38 @@
                     foreach( $_VALUE AS $__VALUE ):
 
                         $EXPLODE__VALUE = $this -> EXPLODE_VAR_ATTR( $__VALUE );
-
+                        
                         if( isset( $EXPLODE__VALUE[ 'ATTR' ][ 'type' ] ) ):
 
-                            if( !filter_var( 
+                            $_TYPE = $EXPLODE__VALUE[ 'ATTR' ][ 'type' ];
 
-                                $this -> ARRAY_INPUTS_METHODS[ $_METHOD ][ $EXPLODE__VALUE[ 'NAME' ] ], 
+                            $_VAR  = $this -> ARRAY_INPUTS_METHODS[ $_METHOD ][ $EXPLODE__VALUE[ 'NAME' ] ];
 
-                                $this -> ARRAY_INPUTS_FILTERS[ $EXPLODE__VALUE[ 'ATTR' ][ 'type' ] ] 
+                            if( $_TYPE == 'integer' OR $_TYPE == 'int' ):
 
-                            ) ):
+                                if( !is_numeric( $_VAR ) ):
 
-                                echo "{$EXPLODE__VALUE[ 'NAME' ]} invalid type";
+                                    echo "{$EXPLODE__VALUE[ 'NAME' ]} invalid type";
 
-                                exit();
+                                    exit();
+
+                                endif;
+
+                            else:
+
+                                if( !filter_var( 
+
+                                    $_VAR, 
+
+                                    $this -> ARRAY_INPUTS_FILTERS[ $_TYPE ] 
+
+                                ) ):
+
+                                    echo "{$EXPLODE__VALUE[ 'NAME' ]} invalid type";
+
+                                    exit();
+
+                                endif;
 
                             endif;
 
