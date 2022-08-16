b76eb97688b4dbed6f87bbf64e880717199aee33
oualid <34518781+ouarrho@users.noreply.github.com>
2022-08-16 18:31:29 +0200

    Update encryptions.php

--- _/security/encryptions.php
+++ _/security/encryptions.php
@@ -7,7 +7,29 @@
 */
 	class ENCRYPTIONS {
 
+		private $arr_hash = [ 
 		
+					'sha256',
+
+					'sha512',
+
+					'whirlpool',
+
+					'ripemd256',
+
+					'ripemd320',
+
+					'snefru',
+
+					'gost',
+
+					'haval256,3',
+
+					'haval256,4',
+
+					'haval256,5' 
+
+				];
 
 	}
 
