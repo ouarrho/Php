08e58bd6f08148a03e756aaee6811e9d407f7519
oualid <34518781+ouarrho@users.noreply.github.com>
2022-08-16 18:05:46 +0200

    Update db-delete.php

--- _/db/db-delete.php
+++ _/db/db-delete.php
@@ -1,8 +1,46 @@
 <?php
 
-    function SQL_DELETE_something(){
+    declare( strict_types = 1 );
 
-        //...
+    class DB_DELETE {
+
+        private $query   = "";
+
+        public  $process = false;
+
+        public function TABLE( string $table = "" ): object {
+
+            $this -> query .= "DELETE FROM {$table}";
+
+            return $this;
+
+        }
+
+        public function WHERE( string $where = "" ): object {
+
+            $this -> query .= " WHERE {$where}";
+
+            return $this;
+
+        }
+
+        public function START( array $parameters = [] ): object {
+
+            global $connect;
+
+            $delete = $connect -> prepare( $this -> query );
+
+            $delete -> execute( $parameters );
+
+            if( $delete ):
+
+                $this -> process = true;
+
+            endif;
+
+            return $this;
+
+        }
 
     }
 
