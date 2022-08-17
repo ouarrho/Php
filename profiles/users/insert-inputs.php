6904c1d28fba4701a4683ce06187cfb6aa48a92d
oualid <34518781+ouarrho@users.noreply.github.com>
2022-08-17 18:45:53 +0200

    Update insert-inputs.php

--- profiles/users/insert-inputs.php
+++ profiles/users/insert-inputs.php
@@ -2,25 +2,12 @@
 
 	const arr_inputs = [
 
-		'SERVER'  => null,
-	
-		'ENV'     => null,
-	
-		'COOKIE'  => null,
-	
-		'SESSION' => null,
-	
-		'FILES'   => null,
-	
-		'REQUEST' => null,
-	
-		'POST'    => null,
+		'GET' => [
 
-		'GET'     => [
+			'id',
 
-			'_FINGERPRINT AS name:fingerPrint type:email max:500 min:100 length:16', 
+			'name'
 
-			'id'
 		]
 
 	];
